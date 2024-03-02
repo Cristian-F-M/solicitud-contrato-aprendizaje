b045b74e1e080d15d8b29bfa61d8bc265bb7f64d
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-02 18:52:29 -0500

    Agregando la condicion unque a la columna email el el modelo usuario

--- app/models/User.py
+++ app/models/User.py
@@ -6,6 +6,6 @@ class User(db.Model):
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_name = db.Column(db.String(50), nullable=False)
-    user_email = db.Column(db.String(50), nullable=False)
+    user_email = db.Column(db.String(50), nullable=False, unique=True)
     user_password = db.Column(db.VARBINARY(256), nullable=False)
     user_rol_id = db.Column(db.Integer, db.ForeignKey("roles.role_id"), nullable=False, default=1)
