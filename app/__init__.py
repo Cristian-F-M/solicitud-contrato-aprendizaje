4e2584f5d831cd6505c9442891a15ae9581b9526
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-02 14:33:29 -0500

    Agregando el import de la ruta auth_routes y haciendo el bluePrint de la misma.

--- app/__init__.py
+++ app/__init__.py
@@ -11,12 +11,12 @@ login_manager = LoginManager()
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.urandom(24)
-    app.config.from_object("config.Config")
+    app.config.from_object("Config.Config")
 
     db.init_app(app)
     login_manager.init_app(app)
 
-    login_manager.login_view = "auth.login"
+    login_manager.login_view = "auth.view_login"
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -25,6 +25,9 @@ def create_app():
         user = User.query.get(int(user_id))
         return user
 
-    # from app.routes import *
-    
+
+
+    from app.routes import auth_routes
+    app.register_blueprint(auth_routes.bp)
+
     return app
