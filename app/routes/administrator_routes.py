54a50bfd8c600a9b0b489bdb9948cf82e26a0e20
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-03 15:37:38 -0500

    Agregando el decorador admin_required en la ruta dashboard.

--- app/routes/administrator_routes.py
+++ app/routes/administrator_routes.py
@@ -2,10 +2,14 @@ from app import db
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, logout_user, login_user, current_user
 from flask import Blueprint, render_template
+from app.decorators.admin_required import admin_required
+
 
 
 bp = Blueprint("administrator", __name__)
 
 @bp.route('/Administrator/Dashboard')
+@login_required
+@admin_required
 def view_dashboard():
     return render_template('administrator/dashboard.html')
