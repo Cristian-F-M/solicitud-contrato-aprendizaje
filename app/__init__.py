d869a4f9515b94316b72be7f2bfb788b48b13800
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-02 19:43:15 -0500

    Añadienlo las rutas administrador, usuario, autenticación al los init

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,6 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         from app.models.User import User
-
         user = User.query.get(int(user_id))
         return user
 
@@ -29,7 +28,9 @@ def create_app():
     def page_not_found(error):
         return render_template('404.html'), 404
 
-    from app.routes import auth_routes
+    from app.routes import auth_routes, administrator_routes, user_routes
     app.register_blueprint(auth_routes.bp)
+    app.register_blueprint(administrator_routes.bp)
+    app.register_blueprint(user_routes.bp)
 
     return app
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 bp = Blueprint('main', __name__)
 
 
-from app.routes import auth_routes
+from app.routes import auth_routes, administrator_routes, user_routes
