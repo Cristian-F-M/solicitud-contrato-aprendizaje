9d7f8f1b550fa958b58d87ecacabf0e5847d6abc
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-09 18:29:25 -0500

    merge con el stash

--- run.py
+++ run.py
@@ -1,14 +1,22 @@
 from app import create_app, db, socketio
 import os
+import sys
+import webbrowser
+from colorama import Fore, Style
 
+debug = False
+port = 8080
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
 
-
-
+if not debug:
+    print(f"{Fore.RED}{Style.BRIGHT}Solo cierra esta ventana cuando ya no vayas a utilizar el programa.{Style.RESET_ALL}")
+    sys.stdout = open('output.log', 'w')
+    sys.stderr = open('error.log', 'w')
+    webbrowser.open(f'http://127.0.0.1:{port}')
 
 if __name__ == "__main__":
     socketio.run(app=app, debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), allow_unsafe_werkzeug=True)
