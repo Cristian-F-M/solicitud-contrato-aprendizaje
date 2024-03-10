25d49d7c75bab024b8d70059c3ce4fe1af7f8bb6
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-10 11:30:22 -0500

    Haciendo que al ejecutar de utilize la variable para el debug

--- run.py
+++ run.py
@@ -19,4 +19,4 @@ if not debug:
     webbrowser.open(f'http://localhost:{port}')
 
 if __name__ == "__main__":
-    socketio.run(app=app, debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), allow_unsafe_werkzeug=True)
+    socketio.run(app=app, debug=debug, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), allow_unsafe_werkzeug=True)
