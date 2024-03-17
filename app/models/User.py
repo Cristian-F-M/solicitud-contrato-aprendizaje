952406c099f834b2abd278d6af3124e703203dab
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-17 16:31:25 -0500

    Añadiendo nuevas columnas al modelo usuario

--- app/models/User.py
+++ app/models/User.py
@@ -1,15 +1,20 @@
 from app import db
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.VARBINARY(256), primary_key=True, default=db.func.uuid_to_bin(db.func.uuid()))
     user_name = db.Column(db.String(50), nullable=False)
     user_email = db.Column(db.String(50), nullable=False, unique=True)
     user_password = db.Column(db.VARBINARY(256), nullable=False)
-    user_role_id = db.Column(db.Integer, db.ForeignKey("roles.role_id"), nullable=False, default=1)
-
+    user_role_id = db.Column(
+        db.Integer, db.ForeignKey("roles.role_id"), nullable=False, default=1
+    )
+    user_user_caprendizaje = db.Column(db.String(25), nullable=True)
+    user_password_caprendizaje = db.Column(db.String(150), nullable=True)
+    user_acept_tyc = db.Column(db.Boolean, nullable=True, default=False)
 
     def get_id(self):
         return self.user_id
