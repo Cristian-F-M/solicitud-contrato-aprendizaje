6efc693b25a055e84abe89b37b09eaa86d968480
Cristian Morales <cfmorales.diaz@gmail.com>
2024-03-25 21:08:33 -0500

    Añadiendo las funciones para agregar la empresa y cambiando nombre de las funciones para blacklist

--- app/models/Company.py
+++ app/models/Company.py
@@ -1,7 +1,9 @@
 import json
-import time
-import uuid
 import os
+import time
+import datetime
+import uuid
+
 from flask_login import current_user
 
 
@@ -36,24 +38,9 @@ class Company:
         with open("/app/data/black_list.json", "w") as file:
             json.dump(data, file, indent=4)
 
-    def add_company_blacklist(
-        self,
-        company_id=str(uuid.uuid4()),
-        company_name=None,
-        company_email_address=None,
-        company_departament=None,
-        company_city=None,
-    ):
-
+    def add_company_blacklist(company):
         Company.init_blacklist()
-
-        company = {
-            "company_id": company_id,
-            "company_name": company_name,
-            "company_email_address": company_email_address,
-            "company_departament": company_departament,
-            "company_city": company_city,
-        }
+        update_company_blacklisted_by_id(company['company_id'], True)
 
         with open("app/data/black_list.json", "r", encoding="utf-8") as file:
             data = json.load(file)
@@ -111,6 +98,8 @@ class Company:
         return empresas_ordenadas
 
     def remove_company_blacklist(company_id):
+        
+        update_company_blacklisted_by_id(company_id, False)
         with open("app/data/black_list.json", "r", encoding="utf-8") as file:
             data = json.load(file)
             for company in data["companies"]:
@@ -121,3 +110,58 @@ class Company:
                 
         with open("app/data/black_list.json", "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4)
+
+
+    def init_companies():
+        with open("app/data/companies.json", "w") as file:
+            
+            formatedDate = time.strftime("%A %d de %B de %Y")
+            
+            data = {
+                "information":{
+                    "cantCompanies": 0, 
+                    "lastUpdated": formatedDate
+                }, 
+                "companies": []
+            }
+            json.dump(data, file, indent=4)
+
+    def add_company(company):
+        blacklist = Company.get_all_companies_blacklist()
+        company['blacklisted'] = company in blacklist
+                
+        with open('app/data/companies.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            formatedDate = time.strftime("%A %d de %B de %Y")
+            data["companies"].append(company)
+            data["information"]["cantCompanies"] = len(data["companies"])
+            data['information']['lastUpdated_f'] = formatedDate
+            
+        with open('app/data/companies.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4)
+
+    def get_companies():
+        with open('app/data/companies.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            return data
+
+    def get_company_by_id(company_id):
+        with open('app/data/companies.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for company in data["companies"]:
+                if company["company_id"] == company_id:
+                    return company
+            return None
+
+
+
+def update_company_blacklisted_by_id(company_id, blacklisted=False):
+    with open('app/data/companies.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        for company in data["companies"]:
+            if company["company_id"] == company_id:
+                company["blacklisted"] = blacklisted
+                break
+
+    with open('app/data/companies.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
