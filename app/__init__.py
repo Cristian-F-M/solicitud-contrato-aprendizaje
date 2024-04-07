b8576c2ac9526fafccbba9b6888385944152733e
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-06 21:31:48 -0500

    Add location_routes.py and update __init__.py to include location_routes

--- app/__init__.py
+++ app/__init__.py
@@ -29,10 +29,11 @@ def create_app():
     def page_not_found(error):
         return render_template('404.html'), 404
 
-    from app.routes import auth_routes, administrator_routes, user_routes, company_routes
+    from app.routes import auth_routes, administrator_routes, user_routes, company_routes, location_routes
     app.register_blueprint(auth_routes.bp)
     app.register_blueprint(administrator_routes.bp)
     app.register_blueprint(user_routes.bp)
     app.register_blueprint(company_routes.bp)
+    app.register_blueprint(location_routes.bp)
 
     return app
