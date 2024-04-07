2c3253e3ffe37559f58090998a8b8954d4888e6e
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-06 22:31:37 -0500

    Update password visibility toggle icons in settings.js

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -7,7 +7,10 @@ const inputCv = document.getElementById('input-cv')
 const dropsDown = document.querySelectorAll('.drop-down h3')
 const isPassword = document.querySelectorAll('.i-password')
 const iMoreOptions = document.querySelectorAll('[data-name="more-options"]')
-
+const SVG_EYES_PASSWORD = {
+    "EYE": `<svg width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="icon icon-tabler icons-tabler-outline icon-tabler-eye" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M10 12a2 2 0 1 0 4 0 2 2 0 0 0-4 0"/><path d="M21 12c-2.4 4-5.4 6-9 6-3.6 0-6.6-2-9-6 2.4-4 5.4-6 9-6 3.6 0 6.6 2 9 6"/></svg>`,
+    "EYE_OFF": `<svg width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="icon icon-tabler icons-tabler-outline icon-tabler-eye-off" viewBox="0 0 24 24"><path stroke="none" d="M0 0h24v24H0z"/><path d="M10.585 10.587a2 2 0 0 0 2.829 2.828"/><path d="M16.681 16.673A8.717 8.717 0 0 1 12 18c-3.6 0-6.6-2-9-6 1.272-2.12 2.712-3.678 4.32-4.674m2.86-1.146A9.055 9.055 0 0 1 12 6c3.6 0 6.6 2 9 6-.666 1.11-1.379 2.067-2.138 2.87M3 3l18 18"/></svg>`
+}
 
 
 asNavSettings.forEach(a => {
@@ -176,7 +179,8 @@ isPassword.forEach(i => {
     i.addEventListener('click', () => {
         let input = i.previousElementSibling
         let type = input.getAttribute('type')
-        i.classList.toggle('bi-eye-slash-fill')
+
+        i.innerHTML = type === "password" ? SVG_EYES_PASSWORD.EYE_OFF : SVG_EYES_PASSWORD.EYE
         input.setAttribute('type', (type === 'password') ? 'text' : 'password')
     })
 })
