64b0832874133c0fcd6a1913a27409bf5e1d2a37
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-13 17:32:42 -0500

    Add  /Administrator/Users route

--- app/routes/administrator_routes.py
+++ app/routes/administrator_routes.py
@@ -1,8 +1,8 @@
-from app import db
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_required, logout_user, login_user, current_user
 from flask import Blueprint, render_template
+from flask_login import login_required
 from app.decorators.admin_required import admin_required
+from app.models.User import User
+from app.models.Company import Company
 
 
 
@@ -13,3 +13,12 @@ bp = Blueprint("administrator", __name__)
 @admin_required
 def view_dashboard():
     return render_template('administrator/dashboard.html')
+
+@bp.route('/Administrator/Users')
+@login_required
+@admin_required
+def view_users():
+    get_companies2 = Company.get_companies()
+    users = User.query.all()
+    return render_template('administrator/users.html', users=users, get_companies2=get_companies2)
+
