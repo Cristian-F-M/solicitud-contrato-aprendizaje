5d46635dff38780097a2d32af0d81a75d6bbf643
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-13 17:47:12 -0500

    add delete_user route

--- app/routes/administrator_routes.py
+++ app/routes/administrator_routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_required
 from app.decorators.admin_required import admin_required
 from app.models.User import User
 from app.models.Company import Company
+from app import db
 
 
 
@@ -22,3 +23,15 @@ def view_users():
     users = User.query.all()
     return render_template('administrator/users.html', users=users, get_companies2=get_companies2)
 
+
+@bp.route('/Administrator/Users/Delete/<string:user_id>')
+@login_required
+@admin_required
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        flash(['information', 'User deleted'], 'flash')
+    
+    return redirect(url_for('administrator.view_users'))
